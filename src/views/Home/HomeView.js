fe88ad716b24995c82abdeabb629d0e6404fe4a6
장재영 <jangjaeyeong@jangjaeyeong-ui-MacBookPro.local>
2025-02-13 23:43:57 +0900

    feat: postUI追加

--- src/views/Home/HomeView.js
+++ src/views/Home/HomeView.js
@@ -8,41 +8,37 @@ export default {
     const posts = [
       {
         id: 1,
-        img: "",
+        img: "../../../images/postImg.png",
         title: "첫 게시물",
-        sub: "첫 게시물에 대한 설명입니다.",
+        sub: "첫 게시물에 대한 설명입니다.지금부터 몇자까지 늘어나는지 테스트를 위해서 한번 길게 써볼까 합니다 같이 테스트를 하면 재미있을 것 같으니 참여를 부탁그리빈다.",
         date: "2025-02-03",
-        comment: 40,
         user: "PickyPeople",
         likeNum: 4
       },
       {
         id: 2,
-        img: "",
+        img: "../../../images/postImg.png",
         title: "첫 게시물",
         sub: "두번째 게시물에 대한 설명입니다.",
         date: "2025-02-03",
-        comment: 40,
         user: "PickyPeople",
         likeNum: 4
       },
       {
         id: 3,
-        img: "",
+        img: "../../../images/postImg.png",
         title: "첫 게시물",
         sub: "세번째 게시물에 대한 설명입니다.",
         date: "2025-02-03",
-        comment: 40,
         user: "PickyPeople",
         likeNum: 4
       },
       {
         id: 4,
-        img: "",
+        img: "../../../images/postImg.png",
         title: "첫 게시물",
         sub: "네번쨰 게시물에 대한 설명입니다.",
         date: "2025-02-03",
-        comment: 40,
         user: "PickyPeople",
         likeNum: 4
       },
--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,23 +1,24 @@
 <template>
   <div>
     <AppHeader @open-login="isLoginModalOpen = true" />
-    <LoginModal :isOpen="isLoginModalOpen" @close-login="isLoginModalOpen = false" />
+    <LoginModal
+      :isOpen="isLoginModalOpen"
+      @close-login="isLoginModalOpen = false"
+    />
     <main class="container">
       <div class="inner">
         <div class="contentBox">
-          <div class="content" v-for="(post, index) in posts" :key="index" >
-            <div class="post">
-              <div class="post-img">
-                <img src="../../../images/postImg.png" alt="">
+          <div v-for="post in posts" :key="post.id" class="post-card">
+            <img src="../../../images/postImg.png" alt="게시물 이미지" class="post-img" />
+            <div class="post-content">
+              <h3 class="post-title">{{ post.title }}</h3>
+              <p class="post-sub">{{ post.sub }}</p>
+              <div class="post-footer">
+                <span class="post-user">{{ post.user }}</span>
+                <span>{{ post.date }}</span>
+                <img src="../../../images/heart.svg" alt="" class="heart">
+                <span class="like">{{ post.likeNum }}</span>
               </div>
-              <div class="post-title">{{ post.title }}</div>
-              <div class="post-subTitle">{{ post.sub }}</div>
-              <div class="post-date">{{ post.date }}</div>
-              <div class="post-comment">{{ post.comment }}</div>
-            </div>
-            <div class="post-explain">
-              <div class="post-user">{{ post.user }}</div>
-              <div class="post-like"></div>
             </div>
           </div>
         </div>
