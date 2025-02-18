40d3eb818c504ed516c0758ae3ecd981e955715c
pickyPeople <jjyjjh33@gmail.com>
2025-02-18 13:10:00 +0900

    feat: 記事の作成ページのcss追加

--- src/views/Write/WriteView.vue
+++ src/views/Write/WriteView.vue
@@ -2,9 +2,12 @@
   <header>
     <div class="inner">
       <div class="user">
-        <h1 class="logo" @click="navigateHome">xlog</h1>
+        <h1 class="logo" @click="navigateHome">Xlog</h1>
         <div class="userIcon">ログアウト</div>
       </div>
+    </div>
+    <div class="content">
+      
     </div>
   </header>
 </template>
